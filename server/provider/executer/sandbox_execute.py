cf190ee635b9d681c1ef8faa90da398afd30f7d3
firekann <leehomin118@gmail.com>
2023-10-08 17:10:09 +0000

    make get and request to json

--- server/provider/executer/sandbox_execute.py
+++ server/provider/executer/sandbox_execute.py
@@ -6,9 +6,9 @@ import subprocess
 
 def run_script(config_data):
     result = []
-    language_name = config_data['language']
-    language_version = config_data['version']
-    executable = flask_config.Config.executable_dictionary[language_name]
+    language = config_data['language']
+    language_version = config_data['language_version']
+    executable = flask_config.Config.executable_dictionary[language]
     is_conmpiled = True
     if not executable:
         try:
@@ -17,9 +17,9 @@ def run_script(config_data):
 
             # arguments를 전달
             additional_args = [flask_config.Config.CODE_FOLDER,
-                               language_name + language_version,
+                               language_version,
                                config_data['code_file_name'],
-                               config_data['code_file_name'].split('.')[0],
+                               'main',
                                ]
 
             # 쉘 명령 실행하면서 결과를 파일로 저장
@@ -35,57 +35,54 @@ def run_script(config_data):
                 flask_config.Config.RESULTS_FOLDER, 'stdout.txt'), 3)[12:])
 
             if exit_code == 0:
-                result.append(f'Success : Compile success')
+                result.append({f'Success': 'Compile success'})
             else:
-                result.append(
-                    f'Error : Compile error (exit code : {exit_code})')
-                result.append(
-                    f'Error : Compile error (error code : {process.returncode})')
+                result.append({f'Error : Compile error': [
+                              f'exit code : {exit_code}', f'error code : {process.returncode} ']})
                 is_conmpiled = False
         except Exception as e:
             result.append(f'Error: Exception')
             result.append(str(e))
             is_conmpiled = False
     if is_conmpiled:
-        try:
-            script_file = os.path.join(
-                flask_config.Config.ROOT_PATH, 'scripts/execute.sh')  # 실행할 스크립트 파일의 경로
+        execute_result = {}
+        # input 수만큼 반복
+        for idx in range(config_data['input_num']):
+            try:
+                script_file = os.path.join(
+                    flask_config.Config.ROOT_PATH, 'scripts/execute.sh')  # 실행할 스크립트 파일의 경로
 
-            # arguments를 전달
-            print(config_data['code_file_name'] if executable else config_data['code_file_name'].split('.')[
-                0])
-            additional_args = [flask_config.Config.CODE_FOLDER,
-                               flask_config.Config.RESULTS_FOLDER,
-                               language_name,
-                               str(config_data['memory_limit']),
-                               str(config_data['time_limit']),
-                               config_data['code_file_name'] if executable else config_data['code_file_name'].split('.')[
-                                   0],
-                               'input.txt',
-                               'output.txt',
-                               'answer.txt',
-                               ]
+                # arguments를 전달
+                additional_args = [flask_config.Config.CODE_FOLDER,
+                                   flask_config.Config.RESULTS_FOLDER,
+                                   language,
+                                   str(config_data['memory_limit']),
+                                   str(config_data['time_limit']),
+                                   config_data['code_file_name'] if executable else 'main',
+                                   f'input{idx}.txt',
+                                   f'output{idx}.txt',
+                                   f'answer{idx}.txt',
+                                   ]
 
-            # 쉘 명령 실행하면서 결과를 파일로 저장
-            with open(os.path.join(flask_config.Config.RESULTS_FOLDER, 'execute.txt'), 'w') as output_file:
-                # 스크립트와 추가 arguments를 함께 전달
-                command = ['sh', script_file] + additional_args
-                process = subprocess.Popen(
-                    command, stdout=output_file, stderr=subprocess.PIPE, universal_newlines=True)
-                _, stderr = process.communicate()
+                # 쉘 명령 실행하면서 결과를 파일로 저장
+                with open(os.path.join(flask_config.Config.RESULTS_FOLDER, f'execute{idx}.txt'), 'w') as output_file:
+                    # 스크립트와 추가 arguments를 함께 전달
+                    command = ['sh', script_file] + additional_args
+                    process = subprocess.Popen(
+                        command, stdout=output_file, stderr=subprocess.PIPE, universal_newlines=True)
+                    _, stderr = process.communicate()
 
-            exit_code = int(util.read_specific_line(os.path.join(
-                flask_config.Config.RESULTS_FOLDER, 'execute.txt'), 4)[12:])
+                exit_code = int(util.read_specific_line(os.path.join(
+                    flask_config.Config.RESULTS_FOLDER, f'execute{idx}.txt'), 4)[12:])
 
-            if process.returncode == 0 and exit_code == 0:
-                result.append(f'Success : Execute success')
-            else:
-                result.append(
-                    f'Error : Compile error (exit code : {exit_code})')
-                result.append(
-                    f'Error : Compile error (error code : {process.returncode})')
-        except Exception as e:
-            result.append(f'Error: Exception')
-            result.append(str(e))
-
+                if exit_code == 0:
+                    execute_result[f'Success{idx}'] = 'Execute success'
+                else:
+                    execute_result['Error'] = [
+                        {f'Execute error at input{idx}.txt': f'exit code : {exit_code}'},
+                        {f'Execute error at input{idx}.txt': f'error code : {process.returncode}'}]
+                    break
+            except Exception as e:
+                execute_result['Error'] = 'Exception : '+str(e)
+    result.append(execute_result)
     return result
